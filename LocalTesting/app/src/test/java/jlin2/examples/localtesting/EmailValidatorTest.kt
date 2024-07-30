9a4cad52a9743a66731720343d09fc3915c0054b
Martin joseph <martinjoseph0028@gmail.com>
2024-07-30 15:02:36 -0400

    Added two local test

---
+++ LocalTesting/app/src/test/java/jlin2/examples/localtesting/EmailValidatorTest.kt
@@ -0,0 +1,18 @@
+package jlin2.examples.localtesting
+
+import org.junit.Assert
+import org.junit.Test
+
+class EmailValidatorTest {
+    @Test
+    fun emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        //Correct email address format, such as 123@abc.com
+        Assert.assertTrue(EmailValidator.isValidEmail("123@abc.com"))
+    }
+
+    @Test
+    fun emailValidator_CorrectEmailSubDomain_ReturnsTrue() {
+        //Correct email address subdomain format, such as 123@abc.co.ca
+        Assert.assertTrue(EmailValidator.isValidEmail("123@abc.co.ca"))
+    }
+}
