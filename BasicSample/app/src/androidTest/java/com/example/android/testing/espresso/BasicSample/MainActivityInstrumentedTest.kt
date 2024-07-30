379f1ca7f92d275aa1a0c51f65200b8446ca321a
k-ananthakrishnan <k.ananthakrishnan1996@gmail.com>
2024-07-30 15:35:44 -0400

    added instrumented test for enter 123 and press Open Activity and Change Text button, and test the string in ShowTextActivity

--- BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityInstrumentedTest.kt
+++ BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityInstrumentedTest.kt
@@ -1,9 +1,17 @@
 package com.example.android.testing.espresso.BasicSample
 
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import org.junit.Rule
 import org.junit.Test
@@ -22,5 +30,14 @@ class MainActivityInstrumentedTest {
         // Verify the text in the TextView
         Espresso.onView(ViewMatchers.withId(R.id.textToBeChanged))
             .check(ViewAssertions.matches(ViewMatchers.withText("123")))
+    }
+
+    @Test
+    fun testOpenActivityAndChangeTextButton() {
+        onView(withId(R.id.editTextUserInput)).perform(typeText("123"), closeSoftKeyboard())
+        onView(withId(R.id.activityChangeTextBtn)).perform(click())
+        onView(withId(R.id.show_text_view)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.show_text_view)).check(matches(withText("123")))
     }
 }
